b5eefd6d668f027f7d2f5175494dee04e16c6f0d
szp <szh1871@163.com>
2019-07-01 18:48:18 +0800

    轮播图

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
         <home-header></home-header>
+        <home-swiper></home-swiper>
+        <div>{{obj[0]}}</div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
 import HomeHeader from './components/header'
+import HomeSwiper from './components/swiper'
 export default {
     data() {
         return {
-
+            obj:[1,2]
         }
     },
     components:{
-        HomeHeader
+        HomeHeader,
+        HomeSwiper
+    },
+    mounted:function(){
+        this.$set(this.obj,0,9)
     }
 }
 </script>
